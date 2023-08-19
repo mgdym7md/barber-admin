fc283e575fba49bb9f3602c7d659a599fbd024c9
Mohamed Magdy <mgdym7md@yahoo.com>
2023-08-20 00:23:10 +0300

    logout fixes

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\PersonalAccessToken;
 use Modules\Booking\Models\Booking;
 use Modules\Booking\Models\BookingService;
 use Modules\Commission\Models\CommissionEarning;
